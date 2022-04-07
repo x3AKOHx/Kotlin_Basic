96c5da53966260c95f48b313b38eb86ed0dda740
Saushev Aleksandr <aleksandr_saushev@epam.com>
2022-04-07 14:37:22 +0300

    Ability to make moves added

--- Connect Four/task/src/connectfour/Menu.kt
+++ Connect Four/task/src/connectfour/Menu.kt
@@ -1,16 +1,52 @@
 package connectfour
 
 class Menu {
-
     private val board = Field()
+
+    private lateinit var playerOne: Player
+    private lateinit var playerTwo: Player
 
     fun mainMenu() {
         println("Connect Four")
         println("First player's name:")
-        val playerOne = Player(readLine()!!)
+        playerOne = Player(readLine()!!, 'o')
         println("Second player's name:")
-        val playerTwo = Player(readLine()!!)
+        playerTwo = Player(readLine()!!, '*')
         board.createCustomField()
-        print("${playerOne.name} VS ${playerTwo.name}\n${board.rows} X ${board.columns} board")
+        println("${playerOne.name} VS ${playerTwo.name}\n${board.rows} X ${board.columns} board")
+        board.drawTheField()
+        gameProcess()
+    }
+
+    private fun gameProcess() {
+        var currentPlayer = playerOne
+        while (makeMove(currentPlayer)) {
+            board.drawTheField()
+            currentPlayer = if (currentPlayer == playerOne) playerTwo else playerOne
+        }
+    }
+
+    private fun makeMove(player: Player): Boolean {
+        println("${player.name}'s turn:")
+        val input = readLine()!!
+        val reg = "[0-9]+".toRegex()
+        if (input == "end") {
+            print("Game over!")
+            return false
+        }
+        if (!input.matches(reg)) {
+            println("Incorrect column number")
+            makeMove(player)
+        } else if (input.toInt() !in 1..board.columns) {
+            println("The column number is out of range (1 - ${board.columns})")
+            makeMove(player)
+        } else if (board.field[0][input.toInt() - 1] != ' ') {
+            println("Column $input is full")
+            makeMove(player)
+        } else {
+            board.putCoin(input.toInt(), player.symbol)
+            return true
+        }
+        return true
     }
 }
